d8fe5bc626a0244c4d475444dbce09c2b11e191e
sha5b <ned.tabulov@gmail.com>
2026-02-26 12:48:20 +0100

    Offload SysSeason array mapping to a background thread to prevent frame hitches

--- systems/time/sys_season.gd
+++ systems/time/sys_season.gd
@@ -38,9 +38,21 @@ func setup(
 	base_moisture_map = base_moist
 
 
+var _is_updating: bool = false
+var _thread_task_id: int = -1
+
 func update(_world: Node, _delta: float) -> void:
 	if time_system == null:
 		return
+	
+	if _is_updating:
+		if _thread_task_id != -1 and WorkerThreadPool.is_task_completed(_thread_task_id):
+			WorkerThreadPool.wait_for_task_completion(_thread_task_id)
+			_thread_task_id = -1
+			_is_updating = false
+			print("Season modifiers applied via thread.")
+		return
+		
 	if time_system.season == _last_season:
 		return
 	_last_season = time_system.season
@@ -50,9 +62,12 @@ func update(_world: Node, _delta: float) -> void:
 func _apply_season_modifiers() -> void:
 	var temp_offset: float = SEASON_TEMP_OFFSET.get(time_system.season, 0.0)
 	var moist_offset: float = SEASON_MOIST_OFFSET.get(time_system.season, 0.0)
+	
+	_is_updating = true
+	_thread_task_id = WorkerThreadPool.add_task(_apply_modifiers_thread.bind(temp_offset, moist_offset), true, "SeasonUpdate")
 
+
+func _apply_modifiers_thread(temp_offset: float, moist_offset: float) -> void:
 	for i in range(base_temperature_map.size()):
 		temperature_map[i] = clampf(base_temperature_map[i] + temp_offset, 0.0, 1.0)
 		moisture_map[i] = clampf(base_moisture_map[i] + moist_offset, 0.0, 1.0)
-
-	print("Season modifiers applied: temp %+.2f, moisture %+.2f" % [temp_offset, moist_offset])
